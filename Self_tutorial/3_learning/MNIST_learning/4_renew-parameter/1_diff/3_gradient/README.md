64486d256d8029adcd1834d162c66b238fa99f73
DoranLyong <cheeryun@gmail.com>
2019-03-31 17:39:25 +0900

    diff

--- Self_tutorial/3_learning/MNIST_learning/4_renew-parameter/1_diff/3_gradient/README.md
+++ Self_tutorial/3_learning/MNIST_learning/4_renew-parameter/1_diff/3_gradient/README.md
@@ -9,7 +9,31 @@
 
 
 ``` python 
+def function(x):  
+    if x.ndim == 1: 
+        return np.sum(x**2)      # f(x0, x1) = x0**2 + x1**2
+
+    else: 
+        return np.sum(x**2, axis=1)
 
 
+def numerical_gradient( f, x ):
+    h = 1e-4    # 0.0001 
+    grad = np.zeros_like(x)      # x와 형상이 같은 zero-배열을 생성 
 
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        """ f(x+h) 계산 """
+        x[idx] = tmp_val + h 
+        fxh1 = f(x)
+
+        """ f(x-h) 계산 """ 
+        x[idx] = tmp_val - h 
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val  # 값 복원 
 ```
+<img src="gradient_code.gif" width=500>
---
+++ Self_tutorial/3_learning/MNIST_learning/4_renew-parameter/1_diff/3_gradient/gradient.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+
+import numpy as np 
+import matplotlib.pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits import mplot3d    # 3D plotting
+
+
+def function(x):  
+    if x.ndim == 1: 
+        return np.sum(x**2)      # f(x0, x1) = x0**2 + x1**2
+
+    else: 
+        return np.sum(x**2, axis=1)
+        
+
+def numerical_gradient( f, x ):
+    h = 1e-4    # 0.0001 
+    grad = np.zeros_like(x)      # x와 형상이 같은 zero-배열을 생성 
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        """ f(x+h) 계산 """
+        x[idx] = tmp_val + h 
+        fxh1 = f(x)
+
+        """ f(x-h) 계산 """ 
+        x[idx] = tmp_val - h 
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val  # 값 복원  
+
+
+
+def main(): 
+    print("Done!")
+
+
+
+if __name__ == "__main__":
+    main()
